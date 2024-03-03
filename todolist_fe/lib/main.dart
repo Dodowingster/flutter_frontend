16d281a6a3d7db0397ae7a9df49a75b00d311a20
Dodowingster <aqqil.azman99@gmail.com>
2024-03-03 18:07:46 +0800

    documentation comments

--- todolist_fe/lib/main.dart
+++ todolist_fe/lib/main.dart
@@ -34,6 +34,20 @@ class _MyHomePageState extends State<MyHomePage> {
   late TextEditingController _titleController;
   late TextEditingController _descriptionController;
 
+/// ------------------------------------------------------------------------------
+///                                                                               |
+///  This overridden initState method is called when the StatefulWidget is        |
+///  inserted into the tree for the first time. It initializes the state of the   |
+///  widget.                                                                      |
+///                                                                               |
+///  Inside this method:                                                          |
+///   - The _tasksFuture variable is assigned the result of ApiService.fetchTasks,|
+///     which fetches tasks from a Django API.                                    |
+///   - Two TextEditingController instances are initialized for handling user     |
+///     input for task title and description.                                     |
+///                                                                               |
+/// ------------------------------------------------------------------------------
+
   @override
   void initState() {
     super.initState();
@@ -41,6 +55,32 @@ class _MyHomePageState extends State<MyHomePage> {
     _titleController = TextEditingController(); // declares variable to Text Editing conroller for title
     _descriptionController = TextEditingController(); // declares variable to Text Editing conroller for description
   }
+
+/// ------------------------------------------------------------------------------
+///                                                                               |
+///  This overridden build method defines the UI layout for displaying a list of  |
+///  tasks. It returns a Scaffold widget with an AppBar displaying the app's      |
+///  title and a body containing a FutureBuilder for handling asynchronous        |
+///  operations.                                                                  |
+///                                                                               |
+///  Inside the FutureBuilder, it checks the connectionState of the snapshot to   |
+///  display appropriate widgets based on different states of the asynchronous    |
+///  operation:                                                                   |
+///   - If the connectionState is waiting, it displays a CircularProgressIndicator|
+///     indicating that data is being fetched.                                    |
+///   - If there's an error, it displays an error message indicating the issue.   |
+///   - If the operation is completed successfully, it builds a ListView to       |
+///     display the list of tasks.                                                |
+///                                                                               |
+///  Each ListTile in the ListView represents a task, with its title, description,|
+///  and completion status displayed. The last modified date is also displayed.   |
+///  Long-pressing on a task triggers the _showTaskOptionsDialog method to display|
+///  options for that task.                                                       |
+///                                                                               |
+///  Additionally, there's a FloatingActionButton to add new tasks, which calls   |
+///  the _showAddTaskDialog method when pressed.                                  |
+///                                                                               |
+/// ------------------------------------------------------------------------------
 
   @override
   Widget build(BuildContext context) {
@@ -103,7 +143,23 @@ class _MyHomePageState extends State<MyHomePage> {
   );
 }
 
-//delete task function
+/// ------------------------------------------------------------------------------
+///                                                                               | 
+///  This function, _deleteTask, is responsible for deleting a task with the      |
+///  provided taskId. It is asynchronous (async) and doesn't return any value     |
+///  (Future<void>).                                                              |
+///                                                                               |
+///  It first checks if the taskId is not null. If it's not null, it attempts to  |
+///  delete the task via the ApiService by calling the deleteTask method with     |
+///  the taskId parameter. If the deletion is successful, it updates the UI with  |
+///  the updated task list by fetching all tasks again and setting the            |
+///  _tasksFuture variable with the updated data.                                 |
+///                                                                               |
+///  If there's an error during the deletion process, it displays a Snackbar      |
+///  with the error message using ScaffoldMessenger.                              |
+///                                                                               |
+///--------------------------------------------------------------------------------
+
   Future<void> _deleteTask(int? taskId) async {
     if (taskId != null) {
       try {
@@ -119,7 +175,29 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-//edit task dialog function, includes checkbox
+/// --------------------------------------------------------------------------------------
+///                                                                                      |
+///  This function, _showEditTaskDialog, displays an AlertDialog allowing the user to    |
+///  edit the details of a task. It is asynchronous (async) and doesn't return any value |
+///  (Future<void>).                                                                     |
+///                                                                                      |
+///  Upon invocation, it initializes the text fields for title and description with the  |
+///  values from the provided task map. It also sets the initial state of the completion |
+///  checkbox based on the 'status' field of the task map.                               |
+///                                                                                      |
+///  The AlertDialog contains fields for editing the title and description of the task,  |
+///  as well as a checkbox for toggling the completion status.                           |
+///                                                                                      |
+///  Users can modify the task details and toggle the completion status. Upon tapping    |
+///  the "Save" button, the function constructs an updatedTask map with the modified     |
+///  values and calls the _updateTask function to update the task via the ApiService.    |
+///  If the update is successful, the dialog is closed.                                  |
+///                                                                                      |
+///  If the user cancels the operation, they can tap the "Cancel" button to dismiss the  |
+///  dialog without making any changes.                                                  |
+///                                                                                      |
+/// --------------------------------------------------------------------------------------
+
   Future<void> _showEditTaskDialog(Map<String, dynamic> task) async {
     _titleController.text = task['title'] ?? '';
     _descriptionController.text = task['description'] ?? '';
@@ -183,7 +261,23 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-//update task function
+/// --------------------------------------------------------------------------------------
+///                                                                                      |
+///  This function, _updateTask, is responsible for updating a task with the provided    |
+///  taskId using the data specified in updatedTask. It is asynchronous (async) and      |
+///  doesn't return any value (Future<void>).                                            |
+///                                                                                      |
+///  It first checks if the taskId is not null. If it's not null, it attempts to update  |
+///  the task via the ApiService by calling the updateTask method with the taskId and    |
+///  updatedTask parameters. If the update is successful, it updates the UI with the     |
+///  new task details by fetching all tasks again and setting the _tasksFuture variable  |
+///  with the updated data.                                                              |
+///                                                                                      |
+///  If there's an error during the update process, it displays a Snackbar with the error|
+///  message using ScaffoldMessenger.                                                    |
+///                                                                                      |
+/// --------------------------------------------------------------------------------------
+
   Future<void> _updateTask(int? taskId, Map<String, dynamic> updatedTask) async {
     if (taskId != null) {
       try {
@@ -199,7 +293,38 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-//show task option function
+/// --------------------------------------------------------------------------------------
+///                                                                                      |
+///  This function, _showTaskOptionsDialog, displays an AlertDialog with options for a   |
+///  given task. It takes a Map<String, dynamic> named task as a parameter, which likely |
+///  represents the details of a task, such as its title and ID.                         |
+///                                                                                      |
+///  The function is asynchronous (async), indicating that it might perform asynchronous |
+///  operations, but it returns nothing (Future<void>).                                  |
+///                                                                                      |
+///  Inside the function, it calls showDialog, which displays a dialog on the screen.    |
+///  The showDialog function takes a BuildContext named context and a builder function.  |
+///  The builder function returns an AlertDialog. This dialog contains a title and a     |
+///  column of options (ListTile widgets).                                               |
+///                                                                                      |
+///  The title of the AlertDialog is set to the title of the task (task['title']), or    |
+///  an empty string if the title is null.                                               |
+///                                                                                      |
+///  The content of the AlertDialog is a Column widget with options for the task. Each   |
+///  option is represented by a ListTile widget.                                         |
+///                                                                                      |
+///  The first ListTile represents the "Edit" option. It contains an icon                |
+///  (Icon(Icons.edit)), the text "Edit", and a onTap callback function. When tapped,    |
+///  it pops the dialog from the stack (Navigator.pop(context)) and calls                |
+///  _showEditTaskDialog(task), presumably to display a dialog for editing the task.     |
+///                                                                                      |
+///  The second ListTile represents the "Delete" option. It contains an icon             |
+///  (Icon(Icons.delete)), the text "Delete", and a onTap callback function. When        |
+///  tapped, it pops the dialog from the stack (Navigator.pop(context)) and calls        |
+///  _deleteTask(task['id']), presumably to delete the task with the provided ID.        |
+///                                                                                      |
+/// --------------------------------------------------------------------------------------
+
   Future<void> _showTaskOptionsDialog(Map<String, dynamic> task) async {
     showDialog(
       context: context,
@@ -210,6 +335,7 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+//First ListTile---------------------------------           
               ListTile(
                 leading: const Icon(Icons.edit),
                 title: const Text('Edit'),
@@ -217,7 +343,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   Navigator.pop(context);
                   _showEditTaskDialog(task);
                 },
-              ),
+              ),  
+//Second ListTile---------------------------------  
               ListTile(
                 leading: const Icon(Icons.delete),
                 title: const Text('Delete'),
@@ -225,7 +352,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   Navigator.pop(context);
                   _deleteTask(task['id']);
                 },
-              ),
+              ), 
             ],
           ),
         );
@@ -233,7 +360,24 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-//add task button function
+/// -------------------------------------------------------------------------------------
+///                                                                                      |
+///  This function, _showAddTaskDialog, shows an AlertDialog allowing the user to add a  |
+///  new task. It is asynchronous (async) and doesn't return any value (Future<void>).   |
+///                                                                                      |
+///  Inside this function, the text controllers for the title and description fields are |
+///  cleared, then an AlertDialog is displayed using showDialog. This AlertDialog        |
+///  contains fields for entering the title and description of the task.                 |
+///                                                                                      |
+///  Users can input data and choose to cancel or confirm adding the task. If the user   |
+///  confirms and both title and description are not empty, a task is created via the    |
+///  ApiService. If successful, the UI is updated with the new task and the dialog is    |
+///  closed. If there's an error during task creation, a Snackbar with the error message |
+///  is displayed. If either title or description is empty, a Snackbar prompts the user  |
+///  to fill in both fields.                                                             |
+///                                                                                      |
+/// -------------------------------------------------------------------------------------
+
   Future<void> _showAddTaskDialog() async {
     _titleController.clear();
     _descriptionController.clear();
